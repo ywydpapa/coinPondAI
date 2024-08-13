52b4a2918cadc6ea5175c1bee8478e47b0530480
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-08-13 17:37:54 +0900

    데이터 조회 추가

--- comm/getUpbit.py
+++ comm/getUpbit.py
@@ -12,12 +12,22 @@ access_key = os.getenv('UPBIT_OPEN_API_ACCESS_KEY')
 secret_key = os.getenv('UPBIT_OPEN_API_SECRET_KEY')
 server_url = os.getenv('UPBIT_OPEN_API_SERVER_URL')
 
+params = {
+	'market': 'KRW-BTC',
+  'states[]': ['done', 'cancel'],
+  'start_time': '2021-01-01T00:00:00+09:00',
+}
+query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+m = hashlib.sha512()
+m.update(query_string)
+query_hash = m.hexdigest()
+
 payload = {
     'access_key': access_key,
     'nonce': str(uuid.uuid4()),
-}
-
-params = {
+    'query_hash': query_hash,
+    'query_hash_alg': 'SHA512',
 }
 
 jwt_token = jwt.encode(payload, secret_key)
@@ -26,6 +36,6 @@ headers = {
   'Authorization': authorization,
 }
 
-res = requests.get(server_url + '/v1/accounts', params=params, headers=headers)
+res = requests.get(server_url + '/v1/orders', params=params, headers=headers)
 res.json()
 print(res.json())
